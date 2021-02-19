1b40b6ffdab5c4130719ae113d370a243e4f7a95
Eagle19243 <eagle19243@gmail.com>
2021-02-19 01:19:06 -0500

    update api url

--- public/js/config.js
+++ public/js/config.js
@@ -1,4 +1,4 @@
-const URL_API = 'http://127.0.0.1:5000';
-// const URL_API = 'http://18.191.32.127:8093';
+// const URL_API = 'http://127.0.0.1:5000';
+const URL_API = 'http://18.191.32.127:8093';
 const MAX_ATTEMPTS = 20;
 const INTERVAL = 30 * 1000;
